5d56f17f99facf498f46bfbc6cc380a20d265b9e
Joe Barnett <joebarnett876@gmail.com>
2021-09-03 18:19:57 +0100

    4.5.1

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -5,9 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+  // we'll use this to ensure we don't try to execute code that has a known
+  // error
+  static boolean hadError = false;
+
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -15,9 +20,53 @@ public class Lox {
       // https://www.freebsd.org/cgi/man.cgi?query=sysexits&apropos=0&sektion=0&manpath=FreeBSD+4.3-RELEASE&format=html
       System.exit(64);
     } else if (args.length == 1) {
-      runFile(args[0]);
+      // runFile(args[0]);
     } else {
-      runPrompt();
+      // runPrompt();
+    }
+  }
+
+  private static void report(int line, String where, String message) {
+    System.err.println("[line" + line + "] Error" + where + ": " + message);
+    hadError = true;
+  }
+
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+
+    // For now, just print the tokens.
+    for (Token token : tokens) {
+      System.out.println(token);
+    }
+  }
+
+  // read a file and execute it
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+
+    if (hadError)
+      System.exit(65);
+  }
+
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      // Allow exiting by hitting ctrl+d to signal end of file
+      if (line == null)
+        break;
+      run(line);
+      // if the user makes a mistake, it shouldn't kill their entire session
+      hadError = false;
     }
   }
 }
