5f1095038c0ef1762568ad9fa46df7e8e151fade
Joe Barnett <joebarnett876@gmail.com>
2021-09-03 16:55:15 +0100

    scanner skeleton

---
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -0,0 +1,23 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.List;
+
+public class Lox {
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+      // we're using exit codes as defined in the UNIX sysexits.h header -
+      // https://www.freebsd.org/cgi/man.cgi?query=sysexits&apropos=0&sektion=0&manpath=FreeBSD+4.3-RELEASE&format=html
+      System.exit(64);
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
+}
