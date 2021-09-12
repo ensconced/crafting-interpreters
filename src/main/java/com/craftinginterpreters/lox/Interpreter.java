eab044a6235fb17b6327ead83e5ac60c4d35ab09
Joe Barnett <joebarnett876@gmail.com>
2021-09-12 15:58:36 +0100

    wip

--- src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -63,6 +63,13 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Object visitAssignExpr(Expr.Assign expr) {
+    Object value = evaluate(expr.value);
+    environment.assign(expr.name, value);
+    return value;
+  }
+
+  @Override
   public Object visitBinaryExpr(Expr.Binary expr) {
     // NB we evaluate the operands in left-to-right order. If those operands have
     // side-effects, that choice is user-visible, so this isn't simply an
