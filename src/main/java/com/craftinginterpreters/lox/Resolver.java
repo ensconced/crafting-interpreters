a711f62b9432ecc6546d52c634c1a58c44046739
Joe Barnett <joebarnett876@gmail.com>
2021-09-18 07:08:17 +0100

    11.3.3

--- src/main/java/com/craftinginterpreters/lox/Resolver.java
+++ src/main/java/com/craftinginterpreters/lox/Resolver.java
@@ -21,6 +21,21 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
     return null;
   }
 
+  @Override
+  public Void visitVarStmt(Stmt.Var stmt) {
+    // binding is split into two steps: declaring and defining
+    declare(stmt.name);
+    if (stmt.initializer != null) {
+      // Resolve the initializer expression in the same scope where the new variable
+      // now exists but is unavilable.
+      resolve(stmt.initializer);
+    }
+    // Once the initializer expression is done, the variable is ready fro prime
+    // time.
+    define(stmt.name);
+    return null;
+  }
+
   void resolve(List<Stmt> statements) {
     for (Stmt statement : statements) {
       resolve(statement);
@@ -41,5 +56,24 @@ class Resolver implements Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
   private void endScope() {
     scopes.pop();
+  }
+
+  private void declare(Token name) {
+    if (scopes.isEmpty()) return;
+
+    // Add the variable to the innermost scope so it shadows
+    // any outer one and so that we know the variable exists. We mark it as "not
+    // ready yet" by binding its name to `false` in the scope map. The value
+    // associated with a key in the scope map represents whether or not we have
+    // finished resolving that variable's initializer.
+    Map<String, Boolean> scope = scopes.peek();
+    scope.put(name.lexeme, false);
+  }
+
+  private void define(Token name) {
+    if (scopes.isEmpty()) return;
+    // Set the variable's value in the scope map to `true` to mark it as fully
+    // initialized and available for use.
+    scopes.peek().put(name.lexeme, true);
   }
 }
