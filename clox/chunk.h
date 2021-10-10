ce0913104eb78660edcc026147ad31a20ae9071a
Joe Barnett <joebarnett876@gmail.com>
2021-10-10 10:04:56 +0100

    14.6.1

--- clox/chunk.h
+++ clox/chunk.h
@@ -10,12 +10,18 @@ typedef struct {
   int count;
   int capacity;
   uint8_t* code;
+  // We store a separate array of integers that parallels the byte code - each
+  // number in the array is the line number in the source for the corresponding
+  // byte in the bytecode. When a runtime error occurs, we look up the line
+  // number at the same index as the current instruction's offset in the code
+  // array.
+  int* lines;
   ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
 
 #endif
