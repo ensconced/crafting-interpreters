555ab319a528c86423b9158db0ce35eaf8c0a092
Joe Barnett <joebarnett876@gmail.com>
2021-10-10 11:23:22 +0100

    15.2

--- clox/vm.c
+++ clox/vm.c
@@ -1,9 +1,56 @@
 #include "vm.h"
 
+#include <stdio.h>
+
 #include "common.h"
+#include "debug.h"
 
 VM vm;
 
 void initVM() {}
 
 void freeVM() {}
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+  for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+    disassembleInstruction(
+        vm.chunk,
+        // a little bit of pointer arithmetic to get the offset
+        (int)(vm.ip - vm.chunk->code));
+#endif
+
+    uint8_t instruction;
+    switch (instruction = READ_BYTE()) {
+      case OP_CONSTANT: {
+        Value constant = READ_CONSTANT();
+        printValue(constant);
+        printf("\n");
+        break;
+      }
+      case OP_RETURN: {
+        return INTERPRET_OK;
+      }
+    }
+  }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+}
+
+InterpretResult interpret(Chunk* chunk) {
+  vm.chunk = chunk;
+  // The instruction pointer is an actual pointer into the middle of the
+  // bytecode array. This is better than using an integer index because it's
+  // faster to dereference a pointer than to look up an element in an array by
+  // index.
+  // We initialize ip by pointing it at the first byte of code in the chunk. We
+  // haven't executed that instruction yet, so ip points to the instruction
+  // *about to be executed*. This will be true during the entire time the VM is
+  // running; the IP always points to the next instruction, not the one
+  // currently being handled.
+  vm.ip = vm.chunk->code;
+  return run();
+}
