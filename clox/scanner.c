318b1c14b2c75f38f5a74fe44fd8cf1735a2d52f
Joe Barnett <joebarnett876@gmail.com>
2021-10-17 19:00:52 +0100

    16.3

--- clox/scanner.c
+++ clox/scanner.c
@@ -18,3 +18,31 @@ void initScanner(const char* source) {
   scanner.current = source;
   scanner.line = 1;
 }
+
+static bool isAtEnd() { return *scanner.current == '\0'; }
+
+static Token makeToken(TokenType type) {
+  Token token;
+  token.type = type;
+  token.start = scanner.start;
+  token.length = (int)(scanner.current - scanner.start);
+  token.line = scanner.line;
+  return token;
+}
+
+static Token errorToken(const char* message) {
+  Token token;
+  token.type = TOKEN_ERROR;
+  // For errors the "lexeme" points to the message string rather than into the
+  // actual source code.
+  token.start = message;
+  token.length = (int)strlen(message);
+  token.line = scanner.line;
+  return token;
+}
+
+Token scanToken() {
+  scanner.start = scanner.current;
+  if (isAtEnd()) return makeToken(TOKEN_EOF);
+  return errorToken("Unexpected character.");
+}
