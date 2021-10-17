151490e7d995e4b3b7de34f4c91c4e75a3095ba7
Joe Barnett <joebarnett876@gmail.com>
2021-10-17 19:35:48 +0100

    16.3.3

--- clox/scanner.c
+++ clox/scanner.c
@@ -26,6 +26,10 @@ static char advance() {
   return scanner.current[-1];
 }
 
+static char peek() { return *scanner.current; }
+
+static char peekNext() { return scanner.current[1]; }
+
 static bool match(char expected) {
   if (isAtEnd()) return false;
   if (*scanner.current != expected) return false;
@@ -53,7 +57,37 @@ static Token errorToken(const char* message) {
   return token;
 }
 
+static void skipWhitespace() {
+  for (;;) {
+    char c = peek();
+    switch (c) {
+      case ' ':
+      case '\r':
+      case '\t':
+        advance();
+        break;
+      case '\n':
+        scanner.line++;
+        advance();
+        break;
+      // Comments aren't technically whitespace but we can sort of treat them
+      // as such.
+      case '/':
+        if (peekNext() == '/') {
+          // A comment goes until the end of the line.
+          while (peek() != '\n' && !isAtEnd()) advance();
+        } else {
+          return;
+        }
+        break;
+      default:
+        return;
+    }
+  }
+}
+
 Token scanToken() {
+  skipWhitespace();
   scanner.start = scanner.current;
   if (isAtEnd()) return makeToken(TOKEN_EOF);
 
