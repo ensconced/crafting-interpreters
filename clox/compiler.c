d869c3f5bc1ad12ea5c22a0b6c80216e9e3ad88a
Joe Barnett <joebarnett876@gmail.com>
2021-10-19 20:09:34 +0100

    17.2.1

--- clox/compiler.c
+++ clox/compiler.c
@@ -5,6 +5,22 @@
 #include "common.h"
 #include "scanner.h"
 
+typedef struct {
+  Token current;
+  Token previous;
+} Parser;
+
+Parser parser;
+
+static void advance() {
+  parser.previous = parser.current;
+  for (;;) {
+    parser.current = scanToken();
+    if (parser.current.type != TOKEN_ERROR) break;
+    errorAtCurrent(parser.current.start);
+  }
+}
+
 void compile(const char* source, Chunk* chunk) {
   initScanner(source);
   advance();
