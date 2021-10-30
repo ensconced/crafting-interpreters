fe50e1ed7a404ec67b988ecab07506336eda02bc
Joe Barnett <joebarnett876@gmail.com>
2021-10-30 22:39:01 +0100

    18.4.1

--- clox/scanner.c
+++ clox/scanner.c
@@ -95,7 +95,7 @@ static void skipWhitespace() {
 static TokenType checkKeyword(int start, int length, const char* rest,
                               TokenType type) {
   if (scanner.current - scanner.start == start + length &&
-      memcmp(scanner.start + start, rest, length)) {
+      memcmp(scanner.start + start, rest, length) == 0) {
     return type;
   }
   return TOKEN_IDENTIFIER;
--- clox/value.h
+++ clox/value.h
@@ -24,7 +24,7 @@ typedef struct {
 // designated initializer syntax for unions - see
 // https://en.cppreference.com/w/c/language/struct_initialization
 #define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
-#define NIL_VAL(value) ((Value){VAl_NIL, {.number = 0}})
+#define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 
 // Macros for unpacking a clox value to get the native C value back out.
