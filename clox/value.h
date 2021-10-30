9b241882def9e4290994ab4be1fb1b27539501f4
Joe Barnett <joebarnett876@gmail.com>
2021-10-30 19:16:57 +0100

    18.3

--- clox/value.h
+++ clox/value.h
@@ -20,6 +20,26 @@ typedef struct {
   } as;
 } Value;
 
+// macros for promoting native C values into clox values - here we're using
+// designated initializer syntax for unions - see
+// https://en.cppreference.com/w/c/language/struct_initialization
+#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL(value) ((Value){VAl_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+
+// Macros for unpacking a clox value to get the native C value back out.
+// Any time we call one of these AS_ macros, we need to guard it behind a call
+// to one of the IS_ macros (see below) first.
+// There is no AS_NIL macro because there is only one nil value, so the value
+// doesn't carry any extra data.
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+// and we have a few macros for checking a value's type
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value) ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
 typedef struct {
   int capacity;
   int count;
