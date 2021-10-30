202e3bf51889b3ddff5be97b377f54dd4b300ac9
Joe Barnett <joebarnett876@gmail.com>
2021-10-30 18:56:51 +0100

    18.2

--- clox/value.h
+++ clox/value.h
@@ -2,7 +2,23 @@
 #define clox_value_h
 #include "common.h"
 
-typedef double Value;
+// The cases here cover each kind of value that has built-in support in the VM.
+typedef enum {
+  VAL_BOOL,
+  VAL_NIL,
+  VAL_NUMBER,
+} ValueType;
+
+// tagged union
+typedef struct {
+  ValueType type;
+  // Using "as" for the name of the union field means is reads nicely, almost
+  // like a cast, when you pull the various values out.
+  union {
+    bool boolean;
+    double number;
+  } as;
+} Value;
 
 typedef struct {
   int capacity;
