ee2a4c497a10312673e33e5a2bff3655cdbd7a6d
Joe Barnett <joebarnett876@gmail.com>
2021-11-17 20:57:13 +0000

    24.3.3

--- clox/compiler.c
+++ clox/compiler.c
@@ -183,13 +183,17 @@ static void patchJump(int offset) {
   currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
-static void endCompiler() {
+static ObjFunction* endCompiler() {
   emitReturn();
+  ObjFunction* function = current->function;
 #ifdef DEBUG_PRINT_CODE
   if (!parser.hadError) {
-    disassembleChunk(currentChunk(), "code");
+    disassembleChunk(currentChunk(), function->name != NULL
+                                         ? function->name->chars
+                                         : "<script>");
   }
 #endif
+  return function;
 }
 
 static void beginScope() { current->scopeDepth++; }
@@ -389,7 +393,16 @@ static void grouping(bool canAssign) {
 static void initCompiler(Compiler* compiler) {
   compiler->localCount = 0;
   compiler->scopeDepth = 0;
+  compiler->function = newFunction();
   current = compiler;
+
+  Local* local = &current->locals[current->localCount++];
+  // The compiler implicitly claims stack slot zero for the VM's own internal
+  // use. We give it an empty name so that the user can't write an identifier
+  // that refers to it.
+  local->depth = 0;
+  local->name.start = "";
+  local->name.length = 0;
 }
 
 static void number(bool canAssign) {
@@ -726,10 +739,10 @@ static void statement() {
   }
 }
 
-bool compile(const char* source, Chunk* chunk) {
+ObjFunction* compile(const char* source) {
   initScanner(source);
   Compiler compiler;
-  initCompiler(&compiler);
+  initCompiler(&compiler, TYPE_SCRIPT);
   compilingChunk = chunk;
   parser.hadError = false;
   parser.panicMode = false;
@@ -737,6 +750,6 @@ bool compile(const char* source, Chunk* chunk) {
   while (!match(TOKEN_EOF)) {
     declaration();
   }
-  endCompiler();
-  return !parser.hadError;
+  ObjFunction* function = endCompiler();
+  return !parser.hadError ? NULL : function;
 }
